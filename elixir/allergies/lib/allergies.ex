d1eeeec835a317e5104ca8dcc576fe844c5cc006
Michał Wieluński <michal@wielunski.net>
2023-01-02 22:46:24 +0100

    Elixir #37

---
+++ elixir/allergies/lib/allergies.ex
@@ -0,0 +1,32 @@
+defmodule Allergies do
+  import Bitwise
+
+  @allergies %{
+    "eggs" => 1,
+    "peanuts" => 2,
+    "shellfish" => 4,
+    "strawberries" => 8,
+    "tomatoes" => 16,
+    "chocolate" => 32,
+    "pollen" => 64,
+    "cats" => 128
+  }
+
+  @doc """
+  List the allergies for which the corresponding flag bit is true.
+  """
+  @spec list(non_neg_integer) :: [String.t()]
+  def list(flags) do
+    for allergen <- Map.keys(@allergies),
+        allergic_to?(flags, allergen),
+        do: allergen
+  end
+
+  @doc """
+  Returns whether the corresponding flag bit in 'flags' is set for the item.
+  """
+  @spec allergic_to?(non_neg_integer, String.t()) :: boolean
+  def allergic_to?(flags, item) do
+    (flags &&& @allergies[item]) > 0
+  end
+end
