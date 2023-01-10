78bec313b07fcba814d044c8159ef6529ee4fd70
Michał Wieluński <michal@wielunski.net>
2023-01-10 02:14:01 +0100

    Elixir - another improvement to Spiral Matrix

--- elixir/spiral-matrix/lib/spiral.ex
+++ elixir/spiral-matrix/lib/spiral.ex
@@ -6,39 +6,18 @@ defmodule Spiral do
   def matrix(0), do: []
 
   def matrix(dimension) do
-    iterate(dimension)
-    |> Enum.with_index(fn {x, y}, i -> {y, x, i + 1} end)
-    |> Enum.sort()
-    |> Enum.map(fn {_, _, n} -> n end)
-    |> Enum.chunk_every(dimension)
+    next_row([], (dimension ** 2)..1 |> Enum.to_list())
   end
 
-  defp iterate(acc \\ [], dimension, point \\ {0, 0}, vector \\ {1, 0})
+  defp next_row(acc, []), do: acc
 
-  defp iterate(acc, d, p, v) do
-    acc = [p | acc]
-
-    cond do
-      move(p, v) |> valid?(d, acc) ->
-        iterate(acc, d, move(p, v), v)
-
-      turn_and_move(p, v) |> valid?(d, acc) ->
-        iterate(acc, d, turn_and_move(p, v), turn(v))
-
-      true ->
-        Enum.reverse(acc)
-    end
+  defp next_row(acc, nums) do
+    acc = acc |> transpose() |> rotate()
+    width = Enum.at(acc, 0, [nil]) |> length()
+    row = Enum.take(nums, width) |> Enum.reverse()
+    next_row([row | acc], Enum.drop(nums, width))
   end
 
-  defp move({x1, y1}, {x2, y2}), do: {x1 + x2, y1 + y2}
-
-  defp turn_and_move(p, v), do: v |> turn() |> then(&move(p, &1))
-
-  defp valid?({x, y}, d, _acc) when x < 0 or x >= d or y < 0 or y >= d, do: false
-  defp valid?(point, _d, acc), do: point not in acc
-
-  defp turn({1, 0}), do: {0, 1}
-  defp turn({0, 1}), do: {-1, 0}
-  defp turn({-1, 0}), do: {0, -1}
-  defp turn({0, -1}), do: {1, 0}
+  defp transpose(list), do: Enum.zip_with(list, & &1)
+  defp rotate(list), do: Enum.map(list, &Enum.reverse/1)
 end
